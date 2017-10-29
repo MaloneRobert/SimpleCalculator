81f5e83bf7fe03f78fc795b3f79cb365df350270
Robert Malone <robert.malone@ColoradoCollege.edu>
2017-10-29 17:59:03 -0500

    Added new comments

--- src/main/SimpleCalc.java
+++ src/main/SimpleCalc.java
@@ -5,25 +5,17 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
- * In this program, the user can type in two real numbers.  The
- * user can click on buttons labeled +, - , *, and / to perform
- * basic arithmetic operations on the numbers.  When the user
- * clicks on a button the answer is displayed. 
- * This class also contains a main() routine, so that
- * it can be run as a stand-alone application.
+ * In this program, the user can use a very simple calculator. I created
+ * this project for fun as a way to expand my java knockledge and also
+ * just to create a calculator. It was made in my early years of java, so
+ * everything is in one class. Obviously, if I were to do this project again
+ * I would separate the classes with polymorphism and see if other design
+ * techniques could fit in well.
  */
 public class SimpleCalc extends JPanel implements ActionListener {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-
-	/**
-     * This main() routine makes it possible to run the SimpleCalc class
-     * as a stand-alone application. 
-     */
     public static void main(String[] args) {
         JFrame window = new JFrame("Simple Calculator");
         SimpleCalc content = new SimpleCalc();
@@ -37,35 +29,20 @@ public class SimpleCalc extends JPanel implements ActionListener {
     //-------------------------------------------------------------------------
 
 
-    private JTextField xInput, yInput;  // Input boxes for the numbers.
+    private JTextField xInput, yInput;
 
-    private JLabel answer;  // JLabel for displaying the answer, or an 
-                            //    error message if appropriate.
-
+    private JLabel answer;
 
     public SimpleCalc() {
 
-        /* Assign a background color to the panel and its
-             content panel.  This color will show through in the gaps
-             between components. */
-
         setBackground(Color.GRAY);
 
-        /* Add an empty border around the panel, which will also
-         * appear in the gray background color. */
-
         setBorder( BorderFactory.createEmptyBorder(5,5,5,5) );
-
-        /* Create the input boxes, and make sure that the background
-             color is white.  (They are likely to be white by default.) */
 
         xInput = new JTextField("0", 10);
         xInput.setBackground(Color.WHITE);
         yInput = new JTextField("0", 10);
         yInput.setBackground(Color.WHITE);
-
-        /* Create panels to hold the input boxes and labels "x =" and
-             "y = ".  These panels use the default FlowLayout layout manager. */
 
         JPanel xPanel = new JPanel();
         xPanel.add( new JLabel(" x = "));
@@ -75,10 +52,11 @@ public class SimpleCalc extends JPanel implements ActionListener {
         yPanel.add( new JLabel(" y = "));
         yPanel.add(yInput);
 
-        /* Create a panel to hold the four buttons for the four
+        /** Here I create a panel to hold the four buttons for the four
              operations.  A GridLayout is used so that the buttons
-             will all have the same size and will fill the panel. 
-             The main panel serves as ActionListener for the buttons. */
+             will all have the same size and will fill the panel.
+             The main panel serves as ActionListener for the buttons.
+				**/
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1,4));
@@ -99,58 +77,36 @@ public class SimpleCalc extends JPanel implements ActionListener {
         divide.addActionListener(this);
         buttonPanel.add(divide);
 
-        /* Create the label for displaying the answer in red
-             on a white background.  The label is set to be
-             "opaque" to make sure that the white background
-             is painted. */
-
         answer = new JLabel("x + y = 0", JLabel.CENTER);
         answer.setForeground(Color.RED);
         answer.setBackground(Color.WHITE);
         answer.setOpaque(true);
-
-        /* Set up the layout for the main panel, using a GridLayout,
-             and add all the components that have been created. */
 
         setLayout(new GridLayout(4,1,3,3));
         add(xPanel);
         add(yPanel);
         add(buttonPanel);
         add(answer);
-
-    }  // end constructor
+    }
 
 
     /**
-     * When the user clicks a button, get the numbers from the input boxes 
+     * When the user clicks a button, get the numbers from the input boxes
      * and perform the operation indicated by the button.  Put the result in
      * the answer label.  If an error occurs, an error message is put in the label.
      */
     public void actionPerformed(ActionEvent evt) {
 
-        double x, y;  // The numbers from the input boxes.
-
-        /* Get a number from the xInput JTextField.  Use 
-             xInput.getText() to get its contents as a String.
-             Convert this String to a double.  The try...catch
-             statement will check for errors in the String.  If 
-             the string is not a legal number, the error message
-             "Illegal data for x." is put into the answer and
-             the actionPerformed() method ends. */
-
+        double x, y;
         try {
             String xStr = xInput.getText();
             x = Double.parseDouble(xStr);
         }
         catch (NumberFormatException e) {
-                // The string xStr is not a legal number.
             answer.setText("Illegal data for x.");
             xInput.requestFocusInWindow();
             return;
         }
-
-        /* Get a number from yInput in the same way. */
-
         try {
             String yStr = yInput.getText();
             y = Double.parseDouble(yStr);
@@ -179,7 +135,7 @@ public class SimpleCalc extends JPanel implements ActionListener {
                 answer.setText( "x / y = " + (x/y) );
         }
 
-    } // end actionPerformed()
+    }
 
 
-}  // end class SimpleCalc
+}  
